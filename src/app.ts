adfdc0e84e841d231619da09f2c2639be505e565
Devansh Vishwakarma <devwish0207@gmail.com>
2024-02-06 14:14:41 +0530

    feat: todo routes created

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Express } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import todoRoutes from './routes'
+// import bodyParser from 'body-parser'
+
+const app: Express = express()
+
+
+
+
+const PORT: string | number = process.env.PORT || 5002
+
+// app.use(bodyParser())
+app.use(cors())
+app.use(todoRoutes)
+
+// replace the host "cluster0.xo006.mongodb.net" with the address of your host generated in MongoDB Atlas.
+// https://docs.mongodb.com/manual/reference/connection-string/
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.l22ywlm.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+const options: any = { useNewUrlParser: true, useUnifiedTopology: true }
+
+
+mongoose
+.connect(uri, options)
+.then(() =>
+    app.listen(PORT, () =>
+        console.log(`Server running on http://localhost:${PORT}`)
+    )
+)
+.catch((error) => {
+    throw error
+})
